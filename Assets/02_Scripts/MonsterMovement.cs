d62c5e1b121fd4227141756293f5042d789b6232
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-09-03 19:10:30 +0900

    [Update] 몬스터가 플레이어와 닿으면 멈추게 하기

--- Assets/02_Scripts/MonsterMovement.cs
+++ Assets/02_Scripts/MonsterMovement.cs
@@ -3,9 +3,27 @@ using UnityEngine;
 public class MonsterMovement : MonoBehaviour
 {
     public float speed;
+    private bool isMoving = true;
 
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (isMoving)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 플레이어와 충돌했을 때
+        if (other.CompareTag("Player"))
+        {
+            StopMovement();
+        }
+    }
+
+    void StopMovement()
+    {
+        isMoving = false;
     }
 }
